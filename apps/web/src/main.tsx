e086253ff08322c93c15fe656aef85784ee39d50
trungngoprogramming <trungngoprogramming@users.noreply.github.com>
2026-09-18 22:32:55 +0700

    delete all

--- apps/web/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App.js";
-import "./index.css";
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<App />);
-}
--- tests/mocks/mock-ratelimit-cli.js
+++
@@ -1,5 +0,0 @@
-// Mock CLI emitting 429 rate limit error message
-import process from "node:process";
-
-process.stderr.write("Error: You have exceeded your rate limit. resets in 45m\n");
-process.exit(1);
